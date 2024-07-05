9d435bafe0b1d7cbb587b8c752e5a8d49349fda8
rojotu2024 <rojotu2024@gmail.com>
2024-07-04 21:33:50 -0500

    Commit inicial
    Se carga le proyecto al repo

---
+++ src/api/controllers/controller.user.js
@@ -0,0 +1,47 @@
+const { authUserS, recuperarContrasenaS, cambiarContrasenaS, agregarCorreoS, reportPedidosS, tabPedidosS, reportZonasS, reportUsuariosS, reportLogsS  } = require("../../domain/service/service.user");
+
+const authUserC = async (req, res) => {
+    const response = await authUserS(req, res);
+    res.status(response.code).json(response)
+}
+
+/** Lista los usuarios que han enviado su coleccion */
+const reportZonasC = async (req, res) => {
+    const response = await reportZonasS(req, res);
+    res.status(response.code).json(response)
+}
+
+/** Lista los usuarios que han enviado su coleccion */
+const reportLogsC = async (req, res) => {
+    const response = await reportLogsS(req, res);
+    res.status(response.code).json(response)
+}
+
+/** Lista los usuarios que han enviado su coleccion */
+const reportUsuariosC = async (req, res) => {
+    const response = await reportUsuariosS(req, res);
+    res.status(response.code).json(response)
+}
+
+/**Lista los usuarios que han enviado su coleccion */
+const tabPedidosC= async (req, res) => {
+    const response = await tabPedidosS(req, res);
+    res.status(response.code).json(response)
+}
+
+const agregarCorreoC = async (req, res) => {
+    const response = await agregarCorreoS(req, res);
+    res.status(response.code).json(response)
+}
+
+const recuperarContrasenaC = async (req, res) => {
+    const response = await recuperarContrasenaS(req, res);
+    res.status(response.code).json(response)
+}
+
+const cambiarContrasenaC = async (req, res) => {
+    const response = await cambiarContrasenaS(req, res);
+    res.status(response.code).json(response)
+}
+
+module.exports = { authUserC, recuperarContrasenaC, cambiarContrasenaC, agregarCorreoC, reportUsuariosC, tabPedidosC, reportZonasC, reportLogsC };
---
+++ src/domain/repository/repository.user.js
@@ -0,0 +1,285 @@
+const bcrypt = require('bcrypt');
+const db = require("../model/index.js");
+const { Op } = require("sequelize");
+const sequelize = require("sequelize");
+
+const logsLogIn = async (userS) => {
+    try {
+        return await db.logs.create(
+            {
+                usuario: userS,
+            })
+    } catch (error) {
+        return false
+    }
+}
+
+const searchUserR = async (userS) => {
+    try {
+        const user = await db.usuario.findOne({
+            where: {
+                usuario: userS
+            }
+        });
+
+        if (user.usuario) {
+            const limites = await buscarLimitesR(user.pais, user.genero, user.rol, user.clima, user.grupo);
+            const hashedPass = await bcrypt.hash(user.contrasena, 8);
+            return {
+                user: user.usuario,
+                nombre: user.nombre_colaborador,
+                password: hashedPass,
+                clima: user.clima,
+                grupo: user.grupo,
+                rol: user.rol,
+                genero: user.genero,
+                pais: user.pais,
+                prendas_superiores: limites[0].prendas_superiores,
+                prendas_inferiores: limites[0].prendas_inferiores,
+                prendas_otros: limites[0].prendas_otros,
+                total: limites[0].total,
+                url_3d: limites[0].url_3d,
+                correo: user.correo,
+                primer_ingreso: user.primer_ingreso,
+                administrador: user.administrador
+            }
+        } else {
+            throw new Error("false")
+        }
+    } catch (error) {
+        return false
+    }
+}
+
+const buscarLimitesR = async (pais, genero, rol, clima, grupo) => {
+    pais = pais.toLowerCase()
+    try {
+        const rols = await db.rol.findAll({
+            where: {
+                clima: {
+                    [Op.substring]: clima
+                },
+                genero: {
+                    [Op.substring]: genero
+                },
+                rol: {
+                    [Op.substring]: rol
+                },
+                grupo: {
+                    [Op.substring]: grupo
+                },
+                pais: {
+                    [Op.substring]: pais
+                }
+            }
+        });
+        return rols
+    } catch (error) {
+        console.log(error)
+        return false
+    }
+}
+
+const tabPedidosR = async () => {
+    try {
+        const orderAttributes = ['usuario', 'pedido'];
+        const userAttributes = ['nro_documento', 'nombre_colaborador', 'correo', 'unidad_negocio', 'ciudad', 'sucursal', 'pais'];
+
+        let orders =
+            await db.pedido.findAll({
+                attributes: orderAttributes,
+                where: {
+                    estado_pedido: "enviado"
+                },
+                // group: ["usuario"],
+                raw: true
+            })
+        let orderByUser = orders.map(order => order?.usuario);
+        let userInfo = {}
+        const users = await db.usuario.findAll({
+            attributes: userAttributes,
+            where: {
+                correo: orderByUser
+            },
+            raw: true
+        });
+        users.map(user => userInfo[user.correo] = user);
+
+        let data = []
+        for (const order of orders) {
+            data.push({
+                ...order,
+                usuario: userInfo[order.usuario]
+            })
+        }
+        return data;
+    } catch (error) {
+        console.log(error)
+        return false;
+    }
+}
+
+const reportLogsR = async () => {
+    try {
+        const logs = await db.logs.findAll();
+        const logsCount = await db.logs.count({
+            attributes: [
+                [sequelize.literal(`DATE("createdAt")`), 'date'],
+                [sequelize.literal(`COUNT(*)`), 'count']
+            ],
+            group: ['date'],
+        })
+        return {
+            logsCount,
+            logs
+        }
+    } catch (error) {
+        console.log(error)
+        return false
+    }
+}
+
+const reportZonasR = async () => {
+    try {
+        const orderAttributes = ['usuario'];
+        const userAttributes = ['correo', 'pais', 'ciudad'];
+
+        let orders =
+            (await db.pedido.findAll({
+                attributes: orderAttributes,
+                where: {
+                    estado_pedido: "enviado"
+                },
+                // group: ["usuario"],
+                raw: true
+            })).map(order => order?.usuario);
+
+        const users = await db.usuario.findAll({
+            attributes: userAttributes,
+            where: {
+                correo: orders
+            },
+            raw: true
+        });
+
+        let countByCountry = {};
+        users.map(user => {
+            countByCountry[user.pais] = {
+                city: user.ciudad,
+                county: user.pais,
+                count: countByCountry[user.pais]?.count ? countByCountry[user.pais].count + 1 : 1
+            }
+        })
+        return countByCountry;
+
+    } catch (error) {
+        console.log(error)
+        return false
+    }
+}
+
+const reportUsuariosR = async () => {
+    let report_usuarios = {
+        usuarios_totales: 0,
+        usuarios_sesion_iniciada: 0,
+        pedidos_enviados: 0,
+        pedidos_pendientes: 0,
+        usuarios_pedido_enviado: {}
+    }
+
+    try {
+        const orderAttributes = ['usuario'];
+        const userAttributes = ['nro_documento', 'nombre_colaborador', 'correo', 'unidad_negocio', 'pais', 'sucursal'];
+        let orders =
+            (await db.pedido.findAll({
+                attributes: orderAttributes,
+                where: {
+                    estado_pedido: "enviado"
+                },
+                raw: true
+            })).map(order => order?.usuario);
+
+        const users = await db.usuario.findAll({
+            attributes: userAttributes,
+            where: {
+                correo: orders
+            },
+            raw: true
+        });
+
+        //Count usuarios que han iniciado sesion
+        const countPedidosPendientes = await db.pedido.count(
+            {
+                where: {
+                    estado_pedido: "pendiente"
+                }
+            }
+        );
+
+        //Count usuarios que han iniciado sesion
+        const countSesionIniciada = await db.usuario.count(
+            {
+                where: {
+                    primer_ingreso: false
+                }
+            }
+        );
+        //Count usuarios que han iniciado sesion
+        const countUsuarios = await db.usuario.count();
+
+        //Count pedidos enviados
+        const countPedidosEnviados = await db.pedido.count(
+            {
+                where: {
+                    estado_pedido: "enviado"
+                }
+            }
+        );
+
+        report_usuarios.usuarios_sesion_iniciada = countSesionIniciada;
+        report_usuarios.usuarios_totales = countUsuarios;
+        report_usuarios.pedidos_enviados = countPedidosEnviados;
+        report_usuarios.pedidos_pendientes= countPedidosPendientes;
+        report_usuarios.usuarios_pedido_enviado = users;
+        return report_usuarios;
+    } catch (error) {
+        console.log(error)
+        return false
+    }
+}
+
+const cambiarContrasenaR = async (usuario, nueva_contrasena) => {
+    const user = await db.usuario.findOne({
+        where: {
+            usuario: usuario
+        }
+    });
+
+    try {
+        if (user.primer_ingreso == true) {
+            await db.usuario.update(
+                {
+                    primer_ingreso: false,
+                },
+                {
+                    where: {
+                        usuario: usuario
+                    }
+                })
+        }
+    } catch (error) {
+        return false
+    }
+
+    return await db.usuario.update(
+        {
+            contrasena: nueva_contrasena,
+        },
+        {
+            where: {
+                usuario: usuario
+            }
+        });
+}
+
+module.exports = { searchUserR, cambiarContrasenaR, logsLogIn, reportUsuariosR, tabPedidosR, reportZonasR, reportLogsR }
