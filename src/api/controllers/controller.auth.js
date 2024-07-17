ffcd9475fd7e03b85e87f7e1f963e9a2c1f8ddfe
rojotu2024 <rojotu2024@gmail.com>
2024-07-16 21:17:58 -0500

    Delete src directory

--- src/api/controllers/controller.auth.js
+++
@@ -1,8 +0,0 @@
-const { authS } = require("../../domain/service/service.auth");
-
-const validateTokenC = async (req, res) => {
-    const response = await authS(req.header("token"));
-    return res.status(response.code).json(response)
-}
-
-module.exports = { validateTokenC };
