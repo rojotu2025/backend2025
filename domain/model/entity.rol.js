4f904217853f8f54d4975753957fb3da765a1642
rojotu2024 <rojotu2024@gmail.com>
2024-07-16 21:20:53 -0500

    Se añade lógica de carrito

---
+++ domain/model/entity.rol.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require('sequelize')
+
+function model(sequelize) {
+    const attributes = {
+        pais: { type: Sequelize.STRING, allowNull: false },
+        genero: { type: Sequelize.STRING, allowNull: false },
+        rol: { type: Sequelize.STRING, allowNull: false },
+        clima: { type: Sequelize.STRING, allowNull: false },
+        grupo: { type: Sequelize.STRING, allowNull: false },
+        prendas_superiores: { type: Sequelize.STRING, allowNull: false },
+        prendas_inferiores: { type: Sequelize.STRING, allowNull: false },
+        prendas_otros: { type: Sequelize.STRING, allowNull: false },
+        total: { type: Sequelize.STRING, allowNull: false },
+        url_3d: { type: Sequelize.STRING, allowNull: false },
+    }
+    const options = {
+        timestamps: false
+    }
+    const _model = sequelize.define('roles', attributes, options)
+    return _model
+}
+
+module.exports = model
