9edd90a7d91dff71cc857cbb7f8765c289e49f21
rojotu2024 <rojotu2024@gmail.com>
2024-07-17 19:12:02 -0500

    Cambios a la estructura de la BD

---
+++ src/api/controllers/controller.prendas.js
@@ -0,0 +1,42 @@
+const { listarPrendasS, buscarPrendaS } = require("../../domain/service/service.prendas");
+
+const listarPrendasC = async (req, res) => {
+    if (req.header("token")) {
+        const response = await listarPrendasS(req.header("token"));
+        return res.status(response.code).json(response)
+    }else{
+        let response = {
+            code: 400,
+            message: "Faltan token de autenticación",
+            data: "Error!"
+        }
+        return res.status(response.code).json(response)
+    }
+}
+
+const buscarPrendaC = async (req, res) => {
+    const { id_prenda } = req.body;
+    if (req.header("token")) {
+
+        if (id_prenda) {
+            const response = await buscarPrendaS(req.header("token"), id_prenda);
+            return res.status(response.code).json(response)
+        }else{
+            let response = {
+                code: 400,
+                message: "Faltan campos!",
+                data: "Error!"
+            }
+            return res.status(response.code).json(response)
+        }
+    }else{
+        let response = {
+            code: 400,
+            message: "Faltan token de autenticación",
+            data: "Error!"
+        }
+        return res.status(response.code).json(response)
+    }
+}
+
+module.exports = { listarPrendasC, buscarPrendaC };
