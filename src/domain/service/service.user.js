72c97c77ab40730be9c3a11936d328429a11fefb
rojotu2024 <rojotu2024@gmail.com>
2024-08-07 22:44:01 -0500

    Update service.user.js

--- src/domain/service/service.user.js
+++ src/domain/service/service.user.js
@@ -321,11 +321,14 @@ const mailS = async (correoUser, subject, texto) => {
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
-        secure: true,
+        secureConnection: false,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASSWORD,
         },
+        tls: {
+            ciphers: 'SSLv3'
+        }
     });
 
     const mailOptions = {
