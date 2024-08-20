1113c51df7c3b881041631b8c330fda8a23dc3de
rojotu2024 <rojotu2024@gmail.com>
2024-08-19 21:35:47 -0500

    Update carrito fix

---
+++ src/domain/repository/repository.prendas.js
@@ -0,0 +1,92 @@
+const { Op } = require("sequelize");
+const db = require("../model/index.js");
+const { buscarParametrosUsuarioR } = require("./repository.user.js");
+
+const listarPrendasR = async (genero, rol, clima, grupo, pais, identidad, tipo) => {
+    try {
+
+        const prendas = await db.prendas.findAll({
+            where: {
+                clima: {
+                    [Op.substring]: clima
+                },
+                genero: genero,
+                identidad: identidad,
+                rol: {
+                    [Op.substring]: rol
+                },
+                grupo: {
+                    [Op.substring]: grupo
+                },
+                pais: {
+                    [Op.substring]: pais
+                },
+                tipo: {
+                    [Op.substring]: tipo
+                }
+            },
+            raw: true
+        })
+
+        if (tipo == "LOOKBOOK" ) {
+            prendas.forEach(prenda => {
+               prenda.tipo = "LOOKBOOK" 
+            });
+        }
+
+        if (tipo == "LOOKBOOK" ) {
+            prendas.forEach(prenda => {
+                prenda.tipo = "OUTFIT" 
+             });
+        }
+
+
+        console.log(prendas);
+        return prendas;
+    } catch (error) {
+        console.log(error);
+        return false
+    }
+
+}
+
+const listarPrendasCarritoR = async (genero, rol, clima, grupo, pais, identidad) => {
+    const prendas = await db.prendas.findAll({
+        // where: {
+        //     clima: {
+        //         [Op.substring]: clima
+        //     },
+        //     genero: genero,
+        //     identidad: identidad,
+        //     rol: {
+        //         [Op.substring]: rol
+        //     },
+        //     grupo: {
+        //         [Op.substring]: grupo
+        //     },
+        //     pais: {
+        //         [Op.substring]: pais
+        //     },
+        // },
+        raw:true
+    })
+
+    return prendas;
+}
+
+const buscarPrendaR = async (id_prenda) => {
+    try {
+        const prenda = await db.prendas.findOne({
+            where: {
+                id: id_prenda,
+            },
+            raw: true
+        });
+        return prenda;
+    } catch (error) {
+        throw new Error(error);
+    }
+
+}
+
+module.exports = { listarPrendasR, buscarPrendaR, listarPrendasCarritoR }
