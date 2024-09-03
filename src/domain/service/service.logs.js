1088c770e68809ced16425e44766b1ef90c76953
rojotu2024 <rojotu2024@gmail.com>
2024-09-03 17:53:02 -0500

    Update service.logs.js

--- src/domain/service/service.logs.js
+++ src/domain/service/service.logs.js
@@ -1,4 +1,5 @@
 const { reportLogsUsersR, reportLogsDayUsersR, reportLogsCartsR } = require("../repository/repository.logs");
+const { searchUserR } = require("../repository/repository.user.js");
 const { authS, decode } = require('./service.auth.js');
 
 // 3 .kpi de ingreso de personas que han ingresaso y quienes no, separado por hombre, mujere y sede
@@ -6,14 +7,14 @@ const reportLogsUsersS = async (token) => {
     let response = {
         code: "",
         message: "",
-        data: {},
+        data: [],
         token: ""
     }
-
-    const isValid = await authS(token);
-    if ((isValid).code == 200 && isValid.data.administrador == true) {
-        const usuario = await isValid;
-        try {
+    try {
+        const isValid = await authS(token);
+        const user = await searchUserR(isValid.data.usuario);
+        if ((isValid).code == 200 && user.dashboard == true) {
+            const usuario = isValid;
             const Logs = await reportLogsUsersR();
             if (Logs) {
                 response.code = 200;
@@ -26,19 +27,17 @@ const reportLogsUsersS = async (token) => {
                 response.data = [];
                 response.token = usuario.token;
             }
-        } catch (error) {
-            console.log(error);
-            
-            response.code = 400
-            response.message = "Ha ocurrido un error al buscar las estadisticas!"
+        } else {
+            response.code = 403
+            response.message = "No autorizado o sesion expirada!"
             response.data = []
-            response.token = usuario.token;
         }
-
-    } else {
-        response.code = 401
-        response.message = "No autorizado o sesion expirada!"
+    } catch (error) {
+        console.log(error);
+        response.code = 400
+        response.message = "Ha ocurrido un error al buscar las estadisticas!"
         response.data = []
+        response.token = token;
     }
     return response
 }
@@ -51,16 +50,16 @@ const reportLogsDayUsersS = async (token) => {
         data: [],
         token: ""
     }
-
-    const isValid = await authS(token);
-    if ((isValid).code == 200 && isValid.data.administrador == true) {
-        const usuario = isValid;
-        try {
+    try {
+        const isValid = await authS(token);
+        const user = await searchUserR(isValid.data.usuario);
+        if ((isValid).code == 200 && user.dashboard == true) {
+            const usuario = isValid;
             const logs = await reportLogsDayUsersR();
             if (logs) {
                 response.code = 200;
                 response.message = "Exito!";
-                response.data = logs;
+                response.data =  logs;
                 response.token = usuario.token;
             } else {
                 response.code = 404;
@@ -68,17 +67,17 @@ const reportLogsDayUsersS = async (token) => {
                 response.data = [];
                 response.token = usuario.token;
             }
-        } catch (error) {
-            response.code = 400
-            response.message = "Ha ocurrido un error al buscar las estadisticas!"
+        } else {
+            response.code = 403
+            response.message = "No autorizado o sesion expirada!"
             response.data = []
-            response.token = usuario.token;
         }
-
-    } else {
-        response.code = 401
-        response.message = "No autorizado o sesion expirada!"
+    } catch (error) {
+        console.log(error);
+        response.code = 400
+        response.message = "Ha ocurrido un error al buscar las estadisticas!"
         response.data = []
+        response.token = token;
     }
     return response
 }
